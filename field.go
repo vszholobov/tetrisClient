d2e308b4b2e485f5abdbeffe9d5c2ab51b5ee11f
vszholobov <seva151201@gmail.com>
2024-02-04 16:12:55 +0300

    Remove unused code

--- field.go
+++ field.go
@@ -40,15 +40,13 @@ func PrintEnemyField(field *big.Int, speed string, score string, cleanCount stri
 	fieldStr := fmt.Sprintf("%b", field)
 	fmt.Print(moveToTopASCII)
 	fmt.Print(moveRightEnemyFieldASCII)
-	for i := 20; i >= 0; i-- {
-		line := fieldStr[i*12 : i*12+12]
+	for i := FieldHeight - 1; i >= 0; i-- {
+		line := fieldStr[i*FieldWidth : i*FieldWidth+FieldWidth]
 		line = strings.ReplaceAll(line, "1", " Ж ")
 		line = strings.ReplaceAll(line, "0", "   ")
 		fmt.Print(line)
 		fmt.Print(moveDownOneLineASCII)
 		fmt.Print(moveRightEnemyFieldASCII)
-		// builder.WriteString(line)
-		// builder.WriteString("\n")
 	}
 	builder.Reset()
 	builder.WriteString("Score: ")
@@ -59,7 +57,6 @@ func PrintEnemyField(field *big.Int, speed string, score string, cleanCount stri
 	builder.WriteString(cleanCount)
 	fmt.Print(builder.String())
 	fmt.Print(moveDownOneLineASCII)
-	// printNextPiece(nextPieceType)
 }
 
 var builder = strings.Builder{}
@@ -67,14 +64,12 @@ var builder = strings.Builder{}
 func PrintSelfField(field *big.Int, speed string, score string, cleanCount string, nextPieceType PieceType) {
 	fieldStr := fmt.Sprintf("%b", field)
 	fmt.Print(moveToTopASCII)
-	for i := 20; i >= 0; i-- {
-		line := fieldStr[i*12 : i*12+12]
+	for i := FieldHeight - 1; i >= 0; i-- {
+		line := fieldStr[i*FieldWidth : i*FieldWidth+FieldWidth]
 		line = strings.ReplaceAll(line, "1", " Ж ")
 		line = strings.ReplaceAll(line, "0", "   ")
 		fmt.Print(line)
 		fmt.Print(moveDownOneLineASCII)
-		// builder.WriteString(line)
-		// builder.WriteString("\n")
 	}
 	builder.Reset()
 	builder.WriteString("Score: ")
@@ -89,8 +84,6 @@ func PrintSelfField(field *big.Int, speed string, score string, cleanCount strin
 }
 
 func printNextPiece(nextPieceType PieceType) {
-	//pieceVal := nextPiece.GetVal()
-
 	fmt.Print(moveToTopASCII + moveRightASCII + " ##############")
 	fmt.Printf(moveDownOneLineASCII + moveRightASCII + " #            #")
 	pieceLines := RepresentationByType[nextPieceType]
@@ -100,18 +93,8 @@ func printNextPiece(nextPieceType PieceType) {
 			curLine = pieceLines[i]
 		}
 		fmt.Printf(moveDownOneLineASCII+moveRightASCII+" #%s#", curLine)
-		//curLine := big.NewInt(0).Lsh(fullLine, uint(i)*FieldWidth)
-		//checkCurrLine := big.NewInt(0).And(curLine, nextPiece.GetVal())
-		//line := fmt.Sprintf("%10b", checkCurrLine)
-		//line = strings.ReplaceAll(line, "1", "Ж")
-		//line = strings.ReplaceAll(line, "0", "")
-		//fmt.Print(moveDownOneLineASCII + moveRightASCII + " #          #")
 	}
 	fmt.Printf(moveDownOneLineASCII + moveRightASCII + " #            #")
 	fmt.Print(moveDownOneLineASCII + moveRightASCII + " ##############")
 	fmt.Print(moveDownAllLinesASCII)
-
-	//fmt.Print(moveDownOneLineASCII + moveRightASCII + " #          #")
-	//fmt.Print(moveDownOneLineASCII + moveRightASCII + " #          #")
-	//fmt.Print(moveDownOneLineASCII + moveRightASCII + " #          #")
 }
